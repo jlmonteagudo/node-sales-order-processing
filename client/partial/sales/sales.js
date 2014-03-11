924114548da598e034c85c2efb05f1712baf08e4
Jose Luis Monteagudo <jlmm@torrecid.com>
2014-03-11 14:17:02 +0100

    client - finished Sales Order Management

--- client/partial/sales/sales.js
+++ client/partial/sales/sales.js
@@ -110,7 +110,7 @@ angular.module('sop.sales', ['ui.select2', 'sop.sales.services'])
 			});
 
 			modalInstance.result.then(function (sale) {
-				$scope.alerts.push({type: 'success', msg: sale._id + ' has been created!'});
+				$scope.alerts.push({type: 'success', msg: 'New sales order has been created!'});
 				$scope.selectPage($scope.currentPage);
 			});
 
@@ -248,264 +248,44 @@ angular.module('sop.sales', ['ui.select2', 'sop.sales.services'])
 
 		};
 
+		$scope.removeLine = function(index) {
+			$scope.sale.lines.splice(index, 1);
+		};
+		
+
 	}])
 
 
 	.controller('SalesNewLineController', ['$scope', '$modalInstance', 'ProductsService', function($scope, $modalInstance, ProductsService) {
 
 		$scope.line = {};
+		$scope.alerts = [];
 		$scope.productsSelectOptions = ProductsService.getOptionsForSelectWidget();
 
-		$scope.save = function () {
-			$scope.line.product.id = $scope.line.product._id;
-			$modalInstance.close($scope.line);
-		};
-
-		$scope.cancel = function () {
-			$modalInstance.dismiss();
-		};
-
-	}]);
-
-
-	/*
-	.controller('SalesNewController', ['$scope', '$http', 'Restangular', 'SalesService', 'CustomersService', '$modal', '$timeout', function($scope, $http, Restangular, SalesService, CustomersService, $modal, $timeout) {
-
-		$scope.customersSelectOptions = CustomersService.getOptionsForSelectWidget();
-
-		$scope.$watch('customer', function(newValue, oldValue) {
-			$scope.espia = newValue;
+		$scope.$watch('line.product', function (val) {
+			if (val === undefined) { return; }
+			$scope.line.price = $scope.line.product.price;
 		});
 
-	}]);
-	*/
+		$scope.save = function (isValid) {
 
-
-
-
-
-
-/*
-	.controller('ProductsListController', ['$scope', '$http', 'ProductsService', '$modal', '$timeout', function($scope, $http, ProductsService, $modal, $timeout) {
-
-		$scope.currentPage = 1;
-		$scope.itemsPerPage = 5;
-		$scope.alerts = [];
-
-
-		var filterTimeout;
-
-		var params = {
-			filters: {},
-			options: {
-				skip: (($scope.currentPage - 1) * $scope.itemsPerPage),
-				limit: $scope.itemsPerPage
+			if (isValid) {
+				$scope.line.product.id = $scope.line.product._id;
+				$modalInstance.close($scope.line);				
 			}
-		};
-
-		var getListProducts = function() {
-			if (filterTimeout) { $timeout.cancel(filterTimeout); }
-			filterTimeout = $timeout(function() {
-				$scope.products = ProductsService.getList(params).$object;
-			}, 250);
-		}
-
-
-		$scope.products = ProductsService.getList(params).$object;
-
-
-		$scope.selectPage = function (pageNumber) {
-			$scope.currentPage = pageNumber;
-			params.options.skip = (($scope.currentPage - 1) * $scope.itemsPerPage);
-			ProductsService.getList(params).then(function(products) {
-				$scope.products = products;
-			});
-		};
-
-		$scope.$watch('filters.description', function (val) {
-			params.filters.description = val;
-			getListProducts();
-		});
-
-
-		$scope.view = function(product) {
-
-			var modalInstance = $modal.open({
-				backdrop: 'static',
-				windowClass: "modal fade in",
-				templateUrl: 'partial/products/products-view.html',
-				controller: 'ProductsViewController',
-				resolve: {
-					product: function () {
-						return product;
-					}
-				}
-			});
-		};
-
-		$scope.edit = function(product) {
-
-			var modalInstance = $modal.open({
-				backdrop: 'static',
-				windowClass: "modal fade in",
-				templateUrl: 'partial/products/products-edit.html',
-				controller: 'ProductsEditController',
-				resolve: {
-					product: function () {
-						return product;
-					}
-				}
-			});
-
-			modalInstance.result.then(function (product) {
-				$scope.alerts.push({type: 'success', msg: product.description + ' has been updated!'});
-			});
-
-		};
-
-		$scope.delete = function(product) {
-
-			var modalInstance = $modal.open({
-				backdrop: 'static',
-				windowClass: "modal fade in",
-				templateUrl: 'partial/products/products-delete.html',
-				controller: 'ProductsDeleteController',
-				resolve: {
-					product: function () {
-						return product;
-					}
-				}
-			});
-
-			modalInstance.result.then(function () {
-				$scope.alerts.push({type: 'success', msg: product.description + ' has been deleted!'});
-				$scope.selectPage($scope.currentPage);
-			});
-
-		};
-
-
-		$scope.new = function(product) {
-
-			var modalInstance = $modal.open({
-				backdrop: 'static',
-				windowClass: "modal fade in",
-				templateUrl: 'partial/products/products-edit.html',
-				controller: 'ProductsNewController',
-				resolve: {
-					products: function () {
-						return $scope.products;
-					}
-				}
-			});
-
-			modalInstance.result.then(function (product) {
-				$scope.alerts.push({type: 'success', msg: product.description + ' has been created!'});
-				$scope.selectPage($scope.currentPage);
-			});
-
-		};
-
-
-		$scope.closeAlert = function(index) {
-			$scope.alerts.splice(index, 1);
-		};
-
-
-	}])
-
-
-	.controller('ProductsViewController', ['$scope', '$modalInstance', 'product', function($scope, $modalInstance, product) {
-
-		$scope.product = product;
-		$scope.close = function () {
-			$modalInstance.close();
-		};
-
-	}])
-
-
-
-	.controller('ProductsNewController', ['$scope', '$modalInstance', 'products', function($scope, $modalInstance, products) {
-
-		$scope.product = {};
-		$scope.alerts = [];
-
-		$scope.save = function () {
-
-			products.post($scope.product)
-				.then(function() {
-					$modalInstance.close($scope.product);
-				}, function(e) {
-					$scope.alerts.push({type: 'danger', msg: e.data.message});
-				});
+			else {
+				$scope.alerts.push({type: 'danger', msg: 'Invalid data'});
+			}
 
 		};
 
 		$scope.cancel = function () {
 			$modalInstance.dismiss();
 		};
-
-		$scope.closeAlert = function(index) {
-			$scope.alerts.splice(index, 1);
-		};
-
-	}])
-
-
-	.controller('ProductsEditController', ['$scope', '$modalInstance', 'product', function($scope, $modalInstance, product) {
-
-		$scope.product = product.clone();
-		$scope.alerts = [];
-
-		$scope.save = function () {
-
-			$scope.product.put()
-				.then(function() {
-					angular.copy($scope.product, product);
-					$modalInstance.close($scope.product);
-				}, function(e) {
-					$scope.alerts.push({type: 'danger', msg: e.data.message});
-				});
-
-		};
-
-		$scope.cancel = function () {
-			$modalInstance.dismiss();
-		};
-
-
-		$scope.closeAlert = function(index) {
-			$scope.alerts.splice(index, 1);
-		};
-
-	}])
-
-
-	.controller('ProductsDeleteController', ['$scope', '$modalInstance', 'product', function($scope, $modalInstance, product) {
-
-		$scope.product = product;
-		$scope.alerts = [];
-
-		$scope.delete = function () {
-
-			$scope.product.remove()
-				.then(function() {
-					$modalInstance.close($scope.product);
-				}, function(e) {
-					$scope.alerts.push({type: 'danger', msg: e.data.message});
-				});
-
-		};
-
-		$scope.cancel = function () {
-			$modalInstance.dismiss();
-		};
-
 
 		$scope.closeAlert = function(index) {
 			$scope.alerts.splice(index, 1);
 		};
 
 	}]);
-*/
+
